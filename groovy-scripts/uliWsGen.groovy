e1501447644d1b5038c686ac1cb87b2af2ff7853
Uli Heller <uli.heller@daemons-point.com>
2012-11-28 13:03:29 +0100

    Some cleanup

--- groovy-scripts/uliWsGen.groovy
+++ groovy-scripts/uliWsGen.groovy
@@ -1,6 +1,7 @@
-// This is a groovy script. You pass the name of a Java interface to it and the
-// script prints some attributes of the interface
-import java.nio.file.Files;
+import java.nio.file.Files; // ... available in Java7 only
+
+import java.lang.reflect.Method;
+
 import javax.tools.JavaCompiler;
 import javax.tools.JavaCompiler.CompilationTask;
 import javax.tools.ToolProvider;
@@ -41,117 +42,262 @@ if (options.h) {
 }
 
 String[] parsedArgs = options.arguments();
+if (parsedArgs.length != 1) {
+  cli.usage();
+  System.err.println("Please specify only one interface name!");
+  System.exit(1);
+}
 
+def additionalClasspath = [];
 ClassLoader cl = this.getClass().getClassLoader();
 if (options.c) {
-  URL url = new File(options.c).toURI().toURL();
-  URL[] urls = new URL[1];
-  urls[0]    = url;
-  cl = new URLClassLoader(urls, cl);
+  cl = new URLClassLoader(new URL[0], cl);
+  additionalClasspath = options.c.tokenize(File.pathSeparator);
+  additionalClasspath.each {
+    cl.addURL(new File(it).toURI().toURL());
+  }
 }
 
-boolean isWebService(Class clazz) {
-  javax.jws.WebService a = clazz.getAnnotation(javax.jws.WebService.class);
-  return a != null;
-}
-
-String className = parsedArgs[0];
-Class clazz = cl.loadClass(className);
-if (! isWebService(clazz)) {
-  System.err.println("Class '${className}' is not a jaxws webservice - annotation @WebService is missing");
-  System.exit(1);
-}
-if (! clazz.isInterface()) {
-  System.err.println("Class '${className}' is not an interface - please use 'wsgen'");
-  System.exit(1);
-}
-
-def deleteList = [];
-File temporaryFolder = Files.createTempDirectory("uliWsGen").toFile();
-deleteList.add(temporaryFolder);
-String[] packageNames = className.tokenize('.')[0..-2];
-File currentFolder = temporaryFolder;
-for (p in packageNames) {
-  currentFolder = new File(currentFolder, p);
-  deleteList.add(currentFolder);
-  currentFolder.mkdir();
-}
-
-String CLASSNAME="uliWsGenTemporaryClass";
-File classFile = new File(currentFolder, "${CLASSNAME}.class");
-deleteList.add(classFile);
-String implClassText = "package ${packageNames.join('.')};\n" \
-  + "@javax.jws.WebService \n" \
-  + "public class ${CLASSNAME} implements ${className} {\n";
-
-for (def m in clazz.getMethods()) {
-  implClassText += "\tpublic ${m.returnType.name} ${m.name}(";
-  int cnt=0; 
-  implClassText += m.parameterTypes.collect{ "${it.name} p${cnt++}" }.join(",");
-  implClassText += ") {\n" \
-    + "\t\t throw new RuntimeException();\n" \
-    + "\t}\n";
-}
-implClassText += "}\n";
-
-// http://www.java2s.com/Code/Java/JDK-6/CompilingfromMemory.htm
-JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-
- /**
-   * A file object used to represent source coming from a string.
-   */
- class JavaSourceFromString extends SimpleJavaFileObject {
-   /**
-     * The source code of this "file".
-     */
-   String code;
-
-    /**
-     * Constructs a new JavaSourceFromString.
-     * @param name the name of the compilation unit represented by this file object
-     * @param code the source code for the compilation unit represented by this file object
-     */
-    JavaSourceFromString(String name, String code) {
-               super(URI.create("string:///" + name.replace('.','/') + JavaFileObject.Kind.SOURCE.extension),
-                     JavaFileObject.Kind.SOURCE);
-               this.code = code;
-    }
-
-    @Override
-    public CharSequence getCharContent(boolean ignoreEncodingErrors) {
-      return code;
-    }
-}
-
-JavaSourceFromString jsfs = new JavaSourceFromString("${packageNames.join('.')}.${CLASSNAME}", implClassText);
-
-Iterable<? extends JavaFileObject> compilationUnits1 = Arrays.asList(jsfs);
-Iterable compilerOptions = Arrays.asList("-d", temporaryFolder.getAbsolutePath(), "-cp", options.c);
-CompilationTask task = compiler.getTask(null, fileManager, null, compilerOptions, null, compilationUnits1)
-boolean fSuccess = task.call();
-fileManager.close();
+SourceInterface sourceInterface = new SourceInterface(className: parsedArgs[0]);
+sourceInterface.check();
 
 int result=0;
-if (!fSuccess) {
-  result=1;
-  System.err.println "Creation of the class file failed!";
-} else if (! classFile.exists()) {
-  result=1;
-  System.err.println "Creation of the class file was successful, but the class file doesn't exist!";
-} else {
-  String cpForProcessBuilder = temporaryFolder.getAbsolutePath()\
-     + File.pathSeparator\
-     + options.c;
-  ProcessBuilder processBuilder = new ProcessBuilder("wsgen", "-cp", cpForProcessBuilder, "-wsdl", "-inlineSchemas", "${packageNames.join('.')}.${CLASSNAME}");
-  //processBuilder.directory(temporaryFolder);
-  Process process = processBuilder.start();
-  result = process.waitFor();
-  System.out.println(process.getErrorStream().getText());
-  System.out.println(process.getInputStream().getText());
-}
+Cleanup cleanup = new Cleanup();
+try {
+  File temporaryFolder = Files.createTempDirectory("uliWsGen").toFile();
 
-for (File f in deleteList.reverse()) {
-  f.delete();
+  String implementationName = Implementation.getImplementationName(sourceInterface.getBaseName());
+  String implementationClassName = Base.packageAndClassName(sourceInterface.getPackageName(), implementationName)
+  Implementation implementation = new Implementation(className: implementationClassName, topLevel: temporaryFolder, cleanup: cleanup, classPath: options.c);
+
+  String implClassText = implementation.getCode(sourceInterface);
+  boolean fSuccess = implementation.compile(implClassText);
+
+  if (!fSuccess) {
+    result=1;
+    System.err.println "Creation of the class file failed!";
+  } else if (! implementation.getClassFile().exists()) {
+    result=1;
+    System.err.println "Creation of the class file was successful, but the class file doesn't exist!";
+  } else {
+    String cpForProcessBuilder = temporaryFolder.getAbsolutePath()\
+       + File.pathSeparator\
+       + options.c;
+    ProcessBuilder processBuilder = new ProcessBuilder("wsgen", "-cp", cpForProcessBuilder, "-wsdl", "-inlineSchemas", implementationClassName);
+    //processBuilder.directory(temporaryFolder);
+    Process process = processBuilder.start();
+    result = process.waitFor();
+    System.out.println(process.getErrorStream().getText());
+    System.out.println(process.getInputStream().getText());
+  }
+} finally {
+  cleanup.cleanup();
 }
 System.exit(result);
+
+// http://docs.oracle.com/javase/7/docs/api/javax/tools/JavaCompiler.html
+/**
+  * A file object used to represent source coming from a string.
+  */
+class JavaSourceFromString extends SimpleJavaFileObject {
+  /**
+    * The source code of this "file".
+    */
+  String code;
+  /**
+   * Constructs a new JavaSourceFromString.
+   * @param name the name of the compilation unit represented by this file object
+   * @param code the source code for the compilation unit represented by this file object
+   */
+  JavaSourceFromString(String name, String code) {
+    super(URI.create("string:///" + name.replace('.','/') + JavaFileObject.Kind.SOURCE.extension), JavaFileObject.Kind.SOURCE);
+    this.code = code;
+  }
+
+  @Override
+  public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+    return code;
+  }
+}
+
+class SourceInterface extends Base {
+  Class clazz;
+
+  public boolean check() {
+    //this.loadClass();
+    if (! isWebService(this.clazz)) {
+      System.err.println("Class '${this.className}' is not a jaxws webservice - annotation @WebService is missing");
+      System.exit(1);
+    }
+    if (! this.clazz.isInterface()) {
+      System.err.println("Class '${this.className}' is not an interface - please use 'wsgen' directly");
+      System.exit(1);
+    }
+  }
+
+  public void setClassName(String className) {
+    super.setClassName(className);
+    this.loadClass();
+  }
+
+  private void loadClass() {
+    this.clazz = this.getClass().getClassLoader().loadClass(this.className);
+  }
+
+  private boolean isWebService(Class clazz) {
+    javax.jws.WebService a = clazz.getAnnotation(javax.jws.WebService.class);
+    return a != null;
+  }
+
+  public Method[] getMethods() {
+    return this.clazz.getMethods();
+  }
+
+  public String getBaseName() {
+    String name = super.getBaseName();
+    return name - "Interface";
+  }
+}
+
+class Implementation extends Base {
+  private final static String IMPL="Impl";
+  public File topLevel;
+  public Cleanup cleanup;
+  public String classPath;
+
+  public void setTopLevel(File topLevel) {
+    this.topLevel = topLevel;
+  }
+
+  public String getBaseName() {
+    String name = super.getBaseName();
+    return name - IMPL;
+  }
+
+  public String getImplementationName() {
+    return Implementation.getImplementationName(this.getBaseName());
+  }
+
+  public String getImplementationNameDotClass() {
+    return this.getImplementationName() + ".class";
+  }
+
+  static public String getImplementationName(String baseName) {
+    return baseName + IMPL;
+  }
+
+  public File getClassFile() {
+    File file = this.topLevel;
+    this.getPackageNames().each {
+      file = new File(file, it);
+    }
+    return new File(file, this.getImplementationNameDotClass());
+  }
+
+  private void createFolders() {
+    cleanup.add(topLevel);
+    String[] packageNames = this.getPackageNames();
+    File currentFolder = topLevel;
+    for (p in packageNames) {
+      currentFolder = new File(currentFolder, p);
+      cleanup.add(currentFolder);
+      currentFolder.mkdir();
+    }
+  }
+
+  public String getCode(SourceInterface sourceInterface) {
+    StringBuffer code = new StringBuffer(256);
+    code.append("""
+      package ${this.getPackageName()};
+      @javax.jws.WebService
+      public class ${this.getImplementationName()} implements ${sourceInterface.className} {
+    """);
+    for (def m in sourceInterface.getMethods()) {
+      code.append("\tpublic ${m.returnType.name} ${m.name}(");
+      int cnt=0; 
+      code.append(m.parameterTypes.collect{ "${it.name} p${cnt++}" }.join(","));
+      code.append(""") {
+          throw new RuntimeException();
+        }
+      """);
+    }
+    code.append("}\n");
+    return code.toString();
+  }
+
+  public boolean compile(String code) {
+    this.createFolders();
+    // http://www.java2s.com/Code/Java/JDK-6/CompilingfromMemory.htm
+    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+
+    JavaSourceFromString jsfs = new JavaSourceFromString(this.className, code);
+
+    Iterable<? extends JavaFileObject> compilationUnits1 = Arrays.asList(jsfs);
+    List<String> compilerOptions = new LinkedList<String>();
+    compilerOptions.add("-d")
+    compilerOptions.add(this.topLevel.getAbsolutePath());
+    if (this.classPath != null && this.classPath.length() > 0) {
+      compilerOptions.add("-cp");
+      compilerOptions.add(this.classPath);
+    }
+    CompilationTask task = compiler.getTask(null, fileManager, null, compilerOptions, null, compilationUnits1)
+    boolean fSuccess = task.call();
+    fileManager.close();
+    return fSuccess;
+  }
+}
+
+class Base {
+  public String className;
+  String[] tokenizedClassName;
+
+  public void setClassName(String className) {
+    this.className = className;
+    this.tokenizeClassName();
+  }
+
+  private void tokenizeClassName() {
+    this.tokenizedClassName = this.className.tokenize('.');
+  }
+
+  public String[] getPackageNames() {
+    String[] packageNames = this.tokenizedClassName[0..-2];
+    return packageNames;
+  }
+
+  public String getBaseName() {
+    String name = this.tokenizedClassName[-1];
+    return name;
+  }
+
+  public String getPackageName() {
+    String packageName = "";
+    if (tokenizedClassName.length > 1) {
+      packageName = this.tokenizedClassName[0..-2].join('.');
+    }
+    return packageName;
+  }
+
+  static public String packageAndClassName(String packageName, String className) {
+    String result = className;
+    if (packageName != null && packageName.length() > 0) {
+      result = packageName + "." + className;
+    }
+    return result;
+  }
+}
+
+class Cleanup {
+  private List<File> filesAndFolders = new LinkedList<File>();
+
+  public void add(File file) {
+    this.filesAndFolders.add(file);
+  }
+
+  public void cleanup() {
+    for (File f in this.filesAndFolders.reverse()) {
+      f.delete();
+    }
+  }
+}
