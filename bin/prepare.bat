f3ad438516d178dea7f8dba2348c0d71d9ed406e
Uli Heller <uli.heller@daemons-point.com>
2012-12-01 13:19:23 +0100

    Create the 3rd-party folder in case it doesn't exist

--- bin/prepare.bat
+++ bin/prepare.bat
@@ -1,14 +1,16 @@
 @echo off
 set D=%~pd0
-
+set TPD=%D%\..\3rd-party
 set GROOVY_VERSION=2.0.5
 set GROOVY_ZIP_BASENAME=groovy-binary-%GROOVY_VERSION%.zip
-set GROOVY_ZIP_ABSOLUTE_PATH=%D%\..\3rd-party\%GROOVY_ZIP_BASENAME%
+set GROOVY_ZIP_ABSOLUTE_PATH=%TPD%\%GROOVY_ZIP_BASENAME%
 set GROOVY_ZIP_DOWNLOAD_URL=http://dist.groovy.codehaus.org/distributions/%GROOVY_ZIP_BASENAME%
+
+if not exist "%TPD%" mkdir "%TPD%"
 
 if exist "%D%\groovy" goto end_groovy
   if exist "%GROOVY_ZIP_ABSOLUTE_PATH%" goto end_groovy_download
-    call %D%\httpcat.bat "%GROOVY_ZIP_DOWNLOAD_URL%" >"%GROOVY_ZIP_ABSOLUTE_PATH%"
+    call "%D%\httpcat.bat" "%GROOVY_ZIP_DOWNLOAD_URL%" >"%GROOVY_ZIP_ABSOLUTE_PATH%"
 :end_groovy_download
   call "%D%\myjar.bat" -xf "%GROOVY_ZIP_ABSOLUTE_PATH%"
   move "groovy-%GROOVY_VERSION%" "%D%\groovy" >NUL
@@ -16,10 +18,10 @@ if exist "%D%\groovy" goto end_groovy
 :end_groovy
 
 set WSDLDIFF_ZIP_BASENAME=soa-model-distribution-1.2.1.RC2.zip
-set WSDLDIFF_ZIP_ABSOLUTE_PATH=%D%\..\3rd-party\%WSDLDIFF_ZIP_BASENAME%
+set WSDLDIFF_ZIP_ABSOLUTE_PATH=%TPD%%WSDLDIFF_ZIP_BASENAME%
 if exist "%D%\wsdldiff" goto end_wsdldiff
   if exist "%WSDLDIFF_ZIP_ABSOLUTE_PATH%" goto end_wsdldiff_download
-    call %D%\httpcat.bat "%WSDLDIFF_ZIP_DOWNLOAD_URL%" >"%WSDLDIFF_ZIP_ABSOLUTE_PATH%"
+    call "%D%\httpcat.bat" "%WSDLDIFF_ZIP_DOWNLOAD_URL%" >"%WSDLDIFF_ZIP_ABSOLUTE_PATH%"
 :end_wsdldiff_download
   mkdir "%D%\wsdldiff"
   setlocal
--- bin/prepare.sh
+++ bin/prepare.sh
@@ -3,10 +3,16 @@
 D="$(dirname "$0")"
 D="$(cd "${D}"; pwd)"
 
+TPD="${D}/../3rd-party"
+
 GROOVY_VERSION="2.0.5"
 GROOVY_ZIP_BASENAME="groovy-binary-${GROOVY_VERSION}.zip"
 GROOVY_ZIP_DOWNLOAD_URL="http://dist.groovy.codehaus.org/distributions/${GROOVY_ZIP_BASENAME}"
-GROOVY_ZIP_ABSOLUTE_PATH="${D}/../3rd-party/${GROOVY_ZIP_BASENAME}"
+GROOVY_ZIP_ABSOLUTE_PATH="${TPD}/${GROOVY_ZIP_BASENAME}"
+
+if [ ! -d "${TPD}" ]; then
+  mkdir -p "${TPD}"
+fi
 
 if [ ! -d "${D}/groovy" ]; then
   if [ ! -s  "${GROOVY_ZIP_ABSOLUTE_PATH}" ]; then
@@ -19,10 +25,10 @@ fi
 
 WSDLDIFF_ZIP_BASENAME=soa-model-distribution-1.2.1.RC2.zip
 WSDLDIFF_ZIP_DOWNLOAD_URL="http://mirror.predic8.com/membrane/soa-model/${WSDLDIFF_ZIP_BASENAME}?"
-WSDLDIFF_ZIP_ABSOLUTE_PATH="${D}/../3rd-party/${WSDLDIFF_ZIP_BASENAME}"
+WSDLDIFF_ZIP_ABSOLUTE_PATH="${TPD}/${WSDLDIFF_ZIP_BASENAME}"
 if [ ! -d "${D}/wsdldiff" ]; then
   if [ ! -s  "${WSDLDIFF_ZIP_ABSOLUTE_PATH}" ]; then
-    "${D}/httpcat.sh "${WSDLDIFF_ZIP_DOWNLOAD_URL}" >"${WSDLDIFF_ZIP_ABSOLUTE_PATH}"
+    "${D}/httpcat.sh" "${WSDLDIFF_ZIP_DOWNLOAD_URL}" >"${WSDLDIFF_ZIP_ABSOLUTE_PATH}"
   fi
   (cd "${D}"; mkdir wsdldiff; cd wsdldiff; "${D}/myjar.sh" -xf "${WSDLDIFF_ZIP_ABSOLUTE_PATH}")
 fi
